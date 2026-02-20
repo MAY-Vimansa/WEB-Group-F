08cdd81f8412ee746f635cc8ad1729a26b060625
MAY-Vimansa <mayvimansa@students.nsbm.ac.lk>
2026-02-20 13:37:26 +0530

    create config.php

---
+++ Required_database_files/config.php
@@ -0,0 +1,8 @@
+<?php
+// Update these values with your real database settings.
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mediconnect');
+?>
